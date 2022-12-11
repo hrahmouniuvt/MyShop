5012cf6337bda4795396ea2b26108d53185b7748
Hicham Rahmouni <hicrahtubi@gmail.com>
2022-12-11 11:12:02 +0200

    Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -56,8 +56,8 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            #messages.success(request, 'You are now logged in.')
-            return redirect('home')
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
         else:
             messages.error(request, 'invalid log in credentials')
             return redirect('login')
@@ -85,3 +85,8 @@ def activate(request, uid65, token):
     else:
         messages.error(request, 'invalid activation link')
         return redirect('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
