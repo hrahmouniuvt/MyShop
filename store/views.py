670aad7dba1e9da63447ef41ed084fd1ec34cf7c
Hicham Rahmouni <hicrahtubi@gmail.com>
2022-12-24 14:01:23 +0200

    Display Rating Stars

--- store/views.py
+++ store/views.py
@@ -49,7 +49,14 @@ def product_detail(request, category_slug, product_slug):
     except OrderProduct.DoesNotExist:
         orderproduct=None
 
-    context = {'single_product': single_product, 'in_cart': in_cart, 'orderproduct': orderproduct,}
+    #get the Reviews
+    reviews =ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
+    context = {'single_product': single_product,
+     'in_cart': in_cart,
+     'orderproduct': orderproduct,
+     'reviews': reviews,
+     }
     return render(request, 'store/product_detail.html', context)
 
 def search(request):
